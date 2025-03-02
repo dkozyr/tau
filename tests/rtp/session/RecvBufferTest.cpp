5c8b4b0bb0d7eb20d8debac20ddbe7c4076d837e
Denys Kozyr <dkozyr@gmail.com>
2025-03-02 18:02:33 +0200

    RecvBuffer test return result

--- tests/rtp/session/RecvBufferTest.cpp
+++ tests/rtp/session/RecvBufferTest.cpp
@@ -13,16 +13,19 @@ namespace rtp::session {
 
 class RecvBufferTest : public ::testing::Test {
 protected:
+    using PacketType = RecvBuffer::PacketType;
+
+protected:
     RecvBufferTest() {
         _recv_buffer.SetCallback([this](Buffer&& packet) {
             _processed_packets.push_back(std::move(packet));
         });
     }
 
-    void PushPackets(const std::vector<uint16_t>& sns) {
+    void PushPackets(const std::vector<uint16_t>& sns, PacketType target_result = PacketType::kOk) {
         for(auto sn : sns) {
             auto packet = CreatePacket(sn);
-            _recv_buffer.Push(std::move(packet), sn);
+            ASSERT_EQ(target_result, _recv_buffer.Push(std::move(packet), sn));
         }
     }
 
@@ -97,63 +100,72 @@ TEST_F(RecvBufferTest, FillBufferOnLostPackets) {
 TEST_F(RecvBufferTest, FillBufferOnLostPacketAndRecovery) {
     uint16_t sn = 1;
     std::vector<uint16_t> sns = {sn};
-    PushPackets(sns);
+    ASSERT_NO_FATAL_FAILURE(PushPackets(sns));
     ASSERT_NO_FATAL_FAILURE(AssertStats(1));
     sn = 3;
     while(sn <= RecvBuffer::kDefaultSize + 1) {
-        PushPackets({sn});
+        ASSERT_NO_FATAL_FAILURE(PushPackets({sn}));
         sns.push_back(sn);
         ASSERT_NO_FATAL_FAILURE(AssertPacket({1}));
         ASSERT_NO_FATAL_FAILURE(AssertStats(sns.size()));
         sn++;
     }
 
-    PushPackets({2});
+    ASSERT_NO_FATAL_FAILURE(PushPackets({2}));
     sns.insert(sns.begin() + 1, 2);
     ASSERT_NO_FATAL_FAILURE(AssertPacket(sns));
     ASSERT_NO_FATAL_FAILURE(AssertStats(sns.size()));
 }
 
 TEST_F(RecvBufferTest, LostPacketsWithOverflow) {
-    PushPackets({65534, 1, 3, 4, 5, 6, 8});
+    ASSERT_NO_FATAL_FAILURE(PushPackets({65534, 1, 3, 4, 5, 6, 8}));
     _recv_buffer.Flush();
     ASSERT_NO_FATAL_FAILURE(AssertPacket({65534, 1, 3, 4, 5, 6, 8}));
     ASSERT_NO_FATAL_FAILURE(AssertStats(7, 0, 4));
 }
 
 TEST_F(RecvBufferTest, Reordered) {
-    PushPackets({1, 3, 5, 7, 9, 10, 8, 6, 4, 2});
+    ASSERT_NO_FATAL_FAILURE(PushPackets({1, 3, 5, 7, 9, 10, 8, 6, 4, 2}));
     ASSERT_NO_FATAL_FAILURE(AssertPacket({1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
     ASSERT_NO_FATAL_FAILURE(AssertStats(10));
 }
 
 TEST_F(RecvBufferTest, ReorderedWithOverflow) {
-    PushPackets({65534, 3, 2, 1, 0, 65535, 6, 5, 7, 4});
+    ASSERT_NO_FATAL_FAILURE(PushPackets({65534, 3, 2, 1, 0, 65535, 6, 5, 7, 4}));
     ASSERT_NO_FATAL_FAILURE(AssertPacket({65534, 65535, 0, 1, 2, 3, 4, 5, 6, 7}));
     ASSERT_NO_FATAL_FAILURE(AssertStats(10));
 }
 
 TEST_F(RecvBufferTest, ReorderedWithDuplicatesAndLost) {
-    PushPackets({40, 100, 50, 80, 80, 110, 40, 110, 50, 40, 100});
+    ASSERT_NO_FATAL_FAILURE(PushPackets({40, 100, 50, 80}));
+    ASSERT_NO_FATAL_FAILURE(PushPackets({80}, PacketType::kDiscarded));
+    ASSERT_NO_FATAL_FAILURE(PushPackets({110}));
+    ASSERT_NO_FATAL_FAILURE(PushPackets({40, 110, 50, 40, 100}, PacketType::kDiscarded));
     _recv_buffer.Flush();
     ASSERT_NO_FATAL_FAILURE(AssertPacket({40, 50, 80, 100, 110}));
     ASSERT_NO_FATAL_FAILURE(AssertStats(11, 6, 66));
 }
 
 TEST_F(RecvBufferTest, Late) {
-    PushPackets({397, 400, 401, 402, 399, 398, 100, 101, 102, 403, 403});
+    ASSERT_NO_FATAL_FAILURE(PushPackets({397, 400, 401, 402, 399, 398}));
+    ASSERT_NO_FATAL_FAILURE(PushPackets({100, 101, 102}, PacketType::kDiscarded));
+    ASSERT_NO_FATAL_FAILURE(PushPackets({403}));
     ASSERT_NO_FATAL_FAILURE(AssertPacket({397, 398, 399, 400, 401, 402, 403}));
-    ASSERT_NO_FATAL_FAILURE(AssertStats(11, 4));
+    ASSERT_NO_FATAL_FAILURE(AssertStats(10, 3));
 }
 
 TEST_F(RecvBufferTest, ResetOnBigJump) {
-    PushPackets({1, 2, 3, 1000, 1001, 1002, 1003});
+    ASSERT_NO_FATAL_FAILURE(PushPackets({1, 2, 3}));
+    ASSERT_NO_FATAL_FAILURE(PushPackets({1000}, PacketType::kReset));
+    ASSERT_NO_FATAL_FAILURE(PushPackets({1001, 1002, 1003}));
     ASSERT_NO_FATAL_FAILURE(AssertPacket({1, 2, 3, 1000, 1001, 1002, 1003}));
     ASSERT_NO_FATAL_FAILURE(AssertStats(7));
 }
 
 TEST_F(RecvBufferTest, ResetOnBigJumpWithOverflow) {
-    PushPackets({50001, 50002, 50003, 0, 1, 2, 3, });
+    ASSERT_NO_FATAL_FAILURE(PushPackets({50001, 50002, 50003}));
+    ASSERT_NO_FATAL_FAILURE(PushPackets({0}, PacketType::kReset));
+    ASSERT_NO_FATAL_FAILURE(PushPackets({1, 2, 3}));
     ASSERT_NO_FATAL_FAILURE(AssertPacket({50001, 50002, 50003, 0, 1, 2, 3}));
     ASSERT_NO_FATAL_FAILURE(AssertStats(7));
 }
@@ -173,9 +185,9 @@ TEST_F(RecvBufferTest, RandomizedWithoutLoss) {
         if(iteration != 0) {
             std::vector<uint16_t> sns_shuffled(sns);
             std::random_shuffle(sns_shuffled.begin(), sns_shuffled.end());
-            PushPackets(sns_shuffled);
+            ASSERT_NO_FATAL_FAILURE(PushPackets(sns_shuffled));
         } else {
-            PushPackets(sns);
+            ASSERT_NO_FATAL_FAILURE(PushPackets(sns));
         }
         ASSERT_NO_FATAL_FAILURE(AssertPacket(sns));
 
