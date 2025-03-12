74a3ea9f95a682d10a77fd825efb3af3cb3d2b46
Denys Kozyr <dkozyr@gmail.com>
2025-03-13 00:27:50 +0200

    String utilities

---
+++ tau/common/String.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <optional>
+
+template<typename R = size_t, typename T>
+std::optional<R> StringToUnsigned(const T& str) {
+    std::optional<R> result;
+    for(auto& c : str) {
+        if(!std::isdigit(c)) {
+            break;
+        }
+        result = result.value_or(0) * 10 + (c - '0');
+    }
+    return result;
+}
+
+template<typename T = size_t>
+std::string ToHexString(T value) {
+    const std::string_view kHexData = "0123456789ABCDEF";
+    std::string result;
+    result.reserve(16);
+    while(value > 0) {
+        result = kHexData[value & 0x0F] + result;
+        value >>= 4;
+    }
+    return result.empty() ? "0" : result;
+}
+
+std::vector<std::string_view> Split(std::string_view str, std::string_view marker, bool ignore_first = false) {
+    size_t prev = 0, pos = 0;
+    std::vector<std::string_view> data;
+    while((pos = str.find(marker, prev)) != std::string::npos) {
+        if(!(ignore_first && (prev == 0))) {
+            data.emplace_back(str.substr(prev, pos - prev));
+        }
+        prev = pos + marker.size();
+    }
+    if((prev != 0) || !str.empty()) {
+        data.emplace_back(str.substr(prev));
+    }
+    return data;
+}
+
+std::vector<std::string_view> Split(const std::string& str, std::string_view marker, bool ignore_first = false) {
+    return Split(std::string_view(str), marker, ignore_first);
+}
---
+++ tests/common/StringTest.cpp
@@ -0,0 +1,73 @@
+#include "tau/common/String.h"
+#include "tests/Common.h"
+
+TEST(StringTest, StringToUnsigned) {
+    ASSERT_EQ(1234567890, StringToUnsigned(std::string_view{"1234567890"}));
+    ASSERT_EQ(12345, StringToUnsigned(std::string_view{"012345-67890"}));
+    ASSERT_EQ(0, StringToUnsigned(std::string_view{"0"}));
+    ASSERT_FALSE(StringToUnsigned(std::string_view{"-12345"}).has_value());
+    ASSERT_FALSE(StringToUnsigned(std::string_view{}).has_value());
+    ASSERT_FALSE(StringToUnsigned(std::string_view{"a"}).has_value());
+}
+
+TEST(StringTest, ToHexString) {
+    ASSERT_EQ("1", ToHexString(1));
+    ASSERT_EQ("FF", ToHexString(255));
+    ASSERT_EQ("807F", ToHexString(127 + 32768));
+    ASSERT_EQ("7FFF1234567890FF", ToHexString(0x7FFF'1234'5678'90FF));
+    ASSERT_EQ("0", ToHexString(-1));
+}
+
+TEST(StringTest, Split) {
+    const std::string_view a = "Hello;world!;User;agent;SessionId";
+    auto tokens = Split(a, ";");
+    ASSERT_EQ(5, tokens.size());
+    ASSERT_EQ("Hello",     tokens[0]);
+    ASSERT_EQ("world!",    tokens[1]);
+    ASSERT_EQ("User",      tokens[2]);
+    ASSERT_EQ("agent",     tokens[3]);
+    ASSERT_EQ("SessionId", tokens[4]);
+}
+
+TEST(StringTest, Split_SingleToken) {
+    const std::string_view a = "Hello world!";
+    auto tokens = Split(a, ";");
+    ASSERT_EQ(1, tokens.size());
+    ASSERT_EQ("Hello world!", tokens[0]);
+}
+
+TEST(StringTest, Split_MarkerAtTheEnd) {
+    const std::string_view a = "Hello world!";
+    auto tokens = Split(a, "!");
+    ASSERT_EQ(2, tokens.size());
+    ASSERT_EQ("Hello world", tokens[0]);
+    ASSERT_EQ(std::string_view{}, tokens[1]);
+}
+
+TEST(StringTest, Split_IgnoreFirst) {
+    const std::string_view a = "Hello world!";
+    {
+        auto tokens = Split(a, "!", true);
+        ASSERT_EQ(1, tokens.size());
+        ASSERT_EQ(std::string_view{}, tokens[0]);
+    }
+    {
+        auto tokens = Split(a, " ", true);
+        ASSERT_EQ(1, tokens.size());
+        ASSERT_EQ("world!", tokens[0]);
+    }
+}
+
+TEST(StringTest, Split_Empty) {
+    {
+        const std::string_view a = {};
+        auto tokens = Split(a, "!", false);
+        ASSERT_EQ(0, tokens.size());
+    }
+    {
+        const std::string_view a = " ";
+        auto tokens = Split(a, " ", true);
+        ASSERT_EQ(1, tokens.size());
+        ASSERT_EQ(std::string_view{}, tokens[0]);
+    }
+}
