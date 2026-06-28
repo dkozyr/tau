c07552fd334f9db831b7361a448486dd4353733f
Denys Kozyr <dkozyr@gmail.com>
2026-06-28 14:05:35 +0300

    ToString

--- tau/asio/ToString.h
+++ tau/asio/ToString.h
@@ -5,11 +5,11 @@
 
 namespace etl {
 
-inline etl::string_stream& operator<<(etl::string_stream& ss, boost::system::error_code ec) {
+inline string_stream& operator<<(string_stream& ss, boost::system::error_code ec) {
     return ss << ec.value() << ", " << ec.message().c_str();
 }
 
-inline etl::string_stream& operator<<(etl::string_stream& ss, const boost::asio::ip::address& address) {
+inline string_stream& operator<<(string_stream& ss, const boost::asio::ip::address& address) {
     const auto ipv4 = address.to_v4().to_uint();
     ss << ((ipv4 >> 24) & 0xFF) << "."
        << ((ipv4 >> 16) & 0xFF) << "."
@@ -19,13 +19,13 @@ inline etl::string_stream& operator<<(etl::string_stream& ss, const boost::asio:
 }
 
 template <typename TProtocol>
-inline etl::string_stream& operator<<(etl::string_stream& ss, const boost::asio::ip::basic_endpoint<TProtocol>& endpoint) {
+inline string_stream& operator<<(string_stream& ss, const boost::asio::ip::basic_endpoint<TProtocol>& endpoint) {
     return ss << endpoint.address() << ":" << endpoint.port();
 }
 
-inline etl::string_stream& operator<<(etl::string_stream& ss, const boost::beast::http::request<boost::beast::http::dynamic_body>& request) {
+inline string_stream& operator<<(string_stream& ss, const boost::beast::http::request<boost::beast::http::dynamic_body>& request) {
     for(const auto& chunk : request.body().data()) {
-        ss << etl::string_view{static_cast<const char*>(chunk.data()), chunk.size()};
+        ss << string_view{static_cast<const char*>(chunk.data()), chunk.size()};
     }
     return ss;
 }
