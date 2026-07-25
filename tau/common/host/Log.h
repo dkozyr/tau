c8344fd74baa12019c092723ea932c17611cdf43
Denys Kozyr <dkozyr@gmail.com>
2026-07-25 13:23:12 +0300

    logging format

--- tau/common/host/Log.h
+++ tau/common/host/Log.h
@@ -4,6 +4,10 @@
 #include <etl/string_stream.h>
 #include <etl/array.h>
 #include <stdio.h>
+#include <sstream>
+#include <thread>
+#include <ctime>
+#include <chrono>
 
 namespace tau {
 
@@ -22,13 +26,19 @@ inline static constexpr etl::array<const char*, 6> g_severity_array = {
 #define DETAIL_LOG_CONTEXT __FILE_NAME__ ":" TO_STRING(__LINE__)
 
 inline static etl::string<2048> g_log_text;
- 
-#define LOG_TRIVIAL(severity, message)                                 \
-    do {                                                               \
-        tau::g_log_text.clear();                                       \
-        etl::string_stream ss_log(tau::g_log_text);                    \
-        ss_log << message;                                             \
-        printf("[%s] [%s] [%s] %s\n", tau::g_severity_array[severity], DETAIL_LOG_CONTEXT, __FUNCTION__, ss_log.str().c_str()); \
+
+#define LOG_TRIVIAL(severity, message)                                   \
+    do {                                                                 \
+        tau::g_log_text.clear();                                         \
+        etl::string_stream ss_log(tau::g_log_text);                      \
+        ss_log << message;                                               \
+        std::stringstream ss;                                            \
+        auto now = std::chrono::steady_clock{}.now().time_since_epoch(); \
+        auto ms = std::chrono::duration<float>(now).count();             \
+        auto id = std::this_thread::get_id();                            \
+        ss << std::fixed << std::setprecision(3) << "[" << ms << "] ";   \
+        ss << "[" << id << "]" ;                                         \
+        printf("%s [%s] [%s] [%s] %s\n", ss.str().c_str(), tau::g_severity_array[severity], DETAIL_LOG_CONTEXT, __FUNCTION__, ss_log.str().c_str()); \
     } while(0);
 
 #define DETAIL_TAU_LOG(severity, message)                              \
